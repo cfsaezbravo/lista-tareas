6649af543ad0ac0003b686b3e3df1e73af8d845b
Camila Sáez <cfsaezbravo@gmail.com>
2017-04-27 13:57:46 -0300

    Guardado 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,10 @@ function obtenerNuevoElemento(){
 
 	input.value = "";
 }
+
+function NuevaTarea (userId, id, title, completed){
+	this.userId = userId;
+	this.id = id;
+	this.title = title;
+	this.completed = completed;
+}
